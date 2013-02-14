84485b0497f3911707a3ee7cae4d4cbb91c6461f
Josh <Jdr0016@uah.edu>
2013-02-14 10:26:22 -0600

    Modified glwidget.cpp to allow code compile
    Modified sections of glwidget.cpp, will look into issues with incorrect
    calls later this afternoon.

--- 6DOFEngineeringScreen/glwidget.cpp
+++ 6DOFEngineeringScreen/glwidget.cpp
@@ -19,9 +19,9 @@ void GLWidget::initializeGL()
 {
     glDisable(GL_TEXTURE_2D);
     glDisable(GL_DEPTH_TEST);
-    glDisable(GL_COLOR_MATERIAL);
+    //glDisable(GL_COLOR_MATERIAL); //glDisable(GL_COLOR_WRITEMASK);?
     glEnable(GL_BLEND);
-    glEnable(GL_POLYGON_SMOOTH);
+    //glEnable(GL_POLYGON_SMOOTH); ??
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     glClearColor(0,0,0,0);
 }
@@ -29,22 +29,22 @@ void GLWidget::initializeGL()
 void GLWidget::resizeGL(int w, int h)
 {
     glViewport(0,0,w,h);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    glOrtho(0,w,0,h,-1.0,1.0);
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
+    //glMatrixMode(GL_PROJECTION);
+    //glLoadIdentity();
+    //glOrtho(0,w,0,h,-1.0,1.0);
+    //glMatrixMode(GL_MODELVIEW);
+    //glLoadIdentity();
 }
 
 void GLWidget::paintGL()
 {
     glClear(GL_COLOR_BUFFER_BIT);
-        glColor3f(1,0,0);
-        glBegin(GL_POLYGON);
-        glVertex2f(0,0);
-        glVertex2f(100,500);
-        glVertex2f(500,100);
-        glEnd();
+        //glColor3f(1,0,0);
+        //glBegin(GL_POLYGON);
+        //glVertex2f(0,0);
+        //glVertex2f(100,500);
+        //glVertex2f(500,100);
+        //glEnd();
 }
 void GLWidget::mousePressEvent(QMouseEvent *event)
 {
--- 6DOFEngineeringScreen/main.cpp
+++ 6DOFEngineeringScreen/main.cpp
@@ -21,7 +21,7 @@ int main(int argc, char *argv[])
 
 
 /* 2-14-13 JR launch point for cube graphic, will be meshed soon */
-/*    QApplication b(argc, argv);
+    QApplication b(argc, argv);
     b.setApplicationName("cube");
     b.setApplicationVersion("0.1");
 #ifndef QT_NO_OPENGL
@@ -32,7 +32,7 @@ int main(int argc, char *argv[])
     QLabel * notifyLabel = new QLabel("OpenGL Support required");
     notifyLabel->show();
 #endif
-    return b.exec(); */
+    return b.exec();
 
 
 
