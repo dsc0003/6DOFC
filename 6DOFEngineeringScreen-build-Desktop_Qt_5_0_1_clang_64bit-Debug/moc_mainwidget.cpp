7c441e4f28f5178d3fac1eeb57fe062892b47e52
John <jpr0002@uah.edu>
2013-04-04 11:15:35 -0500

    Just a sync commit

--- 6DOFEngineeringScreen-build-Desktop_Qt_5_0_1_clang_64bit-Debug/moc_mainwidget.cpp
+++ 6DOFEngineeringScreen-build-Desktop_Qt_5_0_1_clang_64bit-Debug/moc_mainwidget.cpp
@@ -19,8 +19,8 @@
 
 QT_BEGIN_MOC_NAMESPACE
 struct qt_meta_stringdata_MainWidget_t {
-    QByteArrayData data[1];
-    char stringdata[12];
+    QByteArrayData data[13];
+    char stringdata[57];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -29,9 +29,22 @@ struct qt_meta_stringdata_MainWidget_t {
     )
 static const qt_meta_stringdata_MainWidget_t qt_meta_stringdata_MainWidget = {
     {
-QT_MOC_LITERAL(0, 0, 10)
+QT_MOC_LITERAL(0, 0, 10),
+QT_MOC_LITERAL(1, 11, 10),
+QT_MOC_LITERAL(2, 22, 0),
+QT_MOC_LITERAL(3, 23, 1),
+QT_MOC_LITERAL(4, 25, 1),
+QT_MOC_LITERAL(5, 27, 1),
+QT_MOC_LITERAL(6, 29, 2),
+QT_MOC_LITERAL(7, 32, 2),
+QT_MOC_LITERAL(8, 35, 2),
+QT_MOC_LITERAL(9, 38, 2),
+QT_MOC_LITERAL(10, 41, 4),
+QT_MOC_LITERAL(11, 46, 5),
+QT_MOC_LITERAL(12, 52, 3)
     },
-    "MainWidget\0"
+    "MainWidget\0updateCube\0\0x\0y\0z\0R0\0R1\0"
+    "R2\0R3\0roll\0pitch\0yaw\0"
 };
 #undef QT_MOC_LITERAL
 
@@ -41,22 +54,31 @@ static const uint qt_meta_data_MainWidget[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-       0,    0, // methods
+       1,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
        0,       // flags
        0,       // signalCount
 
+ // slots: name, argc, parameters, tag, flags
+       1,   10,   19,    2, 0x0a,
+
+ // slots: parameters
+    QMetaType::Void, QMetaType::Float, QMetaType::Float, QMetaType::Float, QMetaType::Float, QMetaType::Float, QMetaType::Float, QMetaType::Float, QMetaType::Float, QMetaType::Float, QMetaType::Float,    3,    4,    5,    6,    7,    8,    9,   10,   11,   12,
+
        0        // eod
 };
 
 void MainWidget::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
 {
-    Q_UNUSED(_o);
-    Q_UNUSED(_id);
-    Q_UNUSED(_c);
-    Q_UNUSED(_a);
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        MainWidget *_t = static_cast<MainWidget *>(_o);
+        switch (_id) {
+        case 0: _t->updateCube((*reinterpret_cast< float(*)>(_a[1])),(*reinterpret_cast< float(*)>(_a[2])),(*reinterpret_cast< float(*)>(_a[3])),(*reinterpret_cast< float(*)>(_a[4])),(*reinterpret_cast< float(*)>(_a[5])),(*reinterpret_cast< float(*)>(_a[6])),(*reinterpret_cast< float(*)>(_a[7])),(*reinterpret_cast< float(*)>(_a[8])),(*reinterpret_cast< float(*)>(_a[9])),(*reinterpret_cast< float(*)>(_a[10]))); break;
+        default: ;
+        }
+    }
 }
 
 const QMetaObject MainWidget::staticMetaObject = {
@@ -85,6 +107,15 @@ int MainWidget::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     _id = QGLWidget::qt_metacall(_c, _id, _a);
     if (_id < 0)
         return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 1)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 1;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 1)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 1;
+    }
     return _id;
 }
 QT_END_MOC_NAMESPACE
